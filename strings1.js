e8661e87f5c39196b07c6037111c12aae563f34b
kguite <kguite@gmail.com>
2021-04-24 14:28:46 -0700

    8kyu You Only Need One - Beginner

--- strings1.js
+++ strings1.js
@@ -1,6 +1,13 @@
 //8kyu You only need one - Beginner
 
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
 function check(a, x) {
     return a.includes(x)
   }
   
+ 

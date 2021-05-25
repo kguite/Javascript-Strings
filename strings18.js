cf84b43e23940d73ce258aa39002558913bb8c89
kguite <kguite@gmail.com>
2021-05-24 22:38:18 -0700

    MDN example of .charAt method

---
+++ strings18.js
@@ -0,0 +1,11 @@
+//MDN example of .charAt
+
+
+
+
+var anyString = 'Brave new world';
+console.log("The character at index 0   is '" + anyString.charAt()   + "'");
+// No index was provided, used 0 as default
+
+console.log("The character at index 0   is '" + anyString.charAt(0)   + "'");
+The character at index 0   is 'B'
